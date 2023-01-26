7892c3047a6b30e6e40a61c1ed899a6b25e9f0d8
Tutul Kabir <tutulkabir@gmail.com>
2023-01-26 19:54:24 +0530

    innitial setup and updated problem1.js file

---
+++ problem1.js
@@ -0,0 +1,18 @@
+// 01. Creates a function that takes two numbers as arguments and return their sum.
+
+/* function addition(a, b) {
+    //Write Your solution Here
+};
+
+console.log(addition(10, 20)); // 30
+console.log(addition(30, 20)); // 50
+console.log(addition(10, 90)); // 100 */
+
+////====Answer==========//
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(10, 20)); // 30
+console.log(addition(30, 20)); // 50
+console.log(addition(10, 90)); // 100
