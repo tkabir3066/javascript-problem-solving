3e99b09539208f7874874c1751a5f2e011293e5e
Tutul Kabir <tutulkabir@gmail.com>
2023-01-26 20:05:28 +0530

    converted minutes to seconds

---
+++ problem3.js
@@ -0,0 +1,20 @@
+//Converts minutes into seconds.
+
+/* 
+function convert(minutes){
+    //Write Your solution Here
+};
+
+
+console.log(convert(30)); // 1800
+console.log(convert(10)); // 600
+console.log(convert(20)); // 1200
+*/
+
+function convert(minutes) {
+  return minutes * 60;
+}
+
+console.log(convert(30)); // 1800
+console.log(convert(10)); // 600
+console.log(convert(20)); // 1200
