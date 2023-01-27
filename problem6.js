4ff6e83f04fe5e8450e2a32e0f0600ac46a4e62c
Tutul Kabir <tutulkabir@gmail.com>
2023-01-27 21:31:02 +0530

    problem 6 added

---
+++ problem6.js
@@ -0,0 +1,21 @@
+//06. Write Function to return first value of an array.
+
+/* 
+ function getFirstValue(arr) {
+      //Write Your solution Here
+};
+
+
+console.log(getFirstValue(["Saab", "Volvo", "BMW"])); // Saab
+console.log(getFirstValue([3, 5, 1])); // 3
+console.log(getFirstValue(['hello', 'world', 'welcome'])); // hello
+
+*/
+
+function getFirstValue(arr) {
+  return arr[0];
+}
+
+console.log(getFirstValue(["Saab", "Volvo", "BMW"])); // Saab
+console.log(getFirstValue([3, 5, 1])); // 3
+console.log(getFirstValue(["hello", "world", "welcome"])); // hello
