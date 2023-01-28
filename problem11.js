a395001bdb45f72d2fb3524139bfc3517ad5f4af
Tutul Kabir <tutulkabir@gmail.com>
2023-01-28 11:21:10 +0530

    problem 11 square of a number added

---
+++ problem11.js
@@ -0,0 +1,24 @@
+//11. Write function to return the square of a number.
+
+/* 
+ function squared(a){
+      //Write Your solution Here
+};
+
+
+
+console.log(squared(6)); // 36
+console.log(squared(9)); // 81
+console.log(squared(4)); // 16
+
+*/
+
+/// ===Solution====//
+
+function squared(a) {
+  return a * a;
+}
+
+console.log(squared(6)); // 36
+console.log(squared(9)); // 81
+console.log(squared(4)); // 16
