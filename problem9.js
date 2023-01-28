18142d0c8bdb18542661aa4b3a9c937015972bfd
Tutul Kabir <tutulkabir@gmail.com>
2023-01-28 10:04:08 +0530

    problem 9 added

---
+++ problem9.js
@@ -0,0 +1,25 @@
+//Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+/* 
+
+ function isSameNum(num1, num2){
+      //Write Your solution Here
+};
+
+
+console.log(isSameNum(30, 30)); // true
+console.log(isSameNum(20, 40)); // false
+console.log(isSameNum(50, 50)); // true
+
+*/
+
+////===== Solution=========//
+function isSameNum(num1, num2) {
+  let result = num1 === num2 ? true : false;
+
+  return result;
+}
+
+console.log(isSameNum(30, 30)); // true
+console.log(isSameNum(20, 40)); // false
+console.log(isSameNum(50, 50)); // true
